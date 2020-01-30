9af6cab9a85055afd8fc790d382ebd9d554cdc35
Muhammad Abdullah <muhammadabdullah@appsgenii.eu>
2020-01-30 15:52:00 +0500

    changes for the intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
-
+import './Intro.scss'
 export class Intro extends Component {
     render() {
         return (
-            <div>
-                <h3> Hello Into</h3>
+            <div className = "company-intro-section">
+                <div className = 'company-intro'>
+                    <h2> Tech Company</h2>
+                    <p> Web Page Design Template</p>
+                    <p>We are a team of cutting-edge specialists providing high - high quality, 
+                        prompt, value-added solutions to our customers. Our success is attained through 
+                        sincerity in thought, word and activity towards our customers, fellow workers, 
+                        neighborhoods and suppliers. Whould you like to try web Page Design Template?
+                    </p>
+                    <a href='#' className = 'btn btn-primary '> Read More</a>
+                    <a href='#' className = 'btn btn-primary '> Contact Us</a>
+                </div>
             </div>
         )
     }
