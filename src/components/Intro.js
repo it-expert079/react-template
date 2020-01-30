768e1b049374112964287dd0389ea4ed1edda20e
Muhammad Abdullah <muhammadabdullah@appsgenii.eu>
2020-01-30 17:46:23 +0500

    intro secgtion updated

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,16 +4,21 @@ export class Intro extends Component {
     render() {
         return (
             <div className = "company-intro-section">
-                <div className = 'company-intro'>
-                    <h2> Tech Company</h2>
-                    <p> Web Page Design Template</p>
-                    <p>We are a team of cutting-edge specialists providing high - high quality, 
-                        prompt, value-added solutions to our customers. Our success is attained through 
-                        sincerity in thought, word and activity towards our customers, fellow workers, 
-                        neighborhoods and suppliers. Whould you like to try web Page Design Template?
-                    </p>
-                    <a href='#' className = 'btn btn-primary '> Read More</a>
-                    <a href='#' className = 'btn btn-primary '> Contact Us</a>
+                {/* <div class="company-overlay"></div> */}
+                <div className = "company-intro">
+                    <div className ="container company-info">
+                        <div className = 'col-md-12 col-lg-7'>
+                            <h2> Tech Company</h2>
+                            <p> Web Page Design Template</p>
+                            <p>We are a team of cutting-edge specialists providing high - high quality, 
+                                prompt, value-added solutions to our customers. Our success is attained through 
+                                sincerity in thought, word and activity towards our customers, fellow workers, 
+                                neighborhoods and suppliers. Whould you like to try web Page Design Template?
+                            </p>
+                            <a href='#' className = 'btn btn-primary read-more'> Read More</a>
+                            <a href='#' className = 'btn btn-primary contact-us'> Contact Us</a>
+                        </div>   
+                    </div>
                 </div>
             </div>
         )
