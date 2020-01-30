f59f422fbb08b7d8ff904d74a0377bcb0194bed7
Faisal <faisalrana079@gmail.com>
2020-01-30 15:36:49 +0500

    add font-awesome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'react-fontawesome'
 import Header from './components/Header';
 import Intro from './components/Intro';
 
