0e7eaba4ebd5f4f3ee1a714cd77c6c369eaf2cb8
Muhammad Abdullah <muhammadabdullah@appsgenii.eu>
2020-01-31 17:32:22 +0500

    axios added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header';
 import Intro from './components/Intro';
 import Technologies from './components/Technologies';
 import Article from './components/Article';
+import Countries from './components/Countries';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Intro />
       <Technologies />
       <Article />
+      <Countries />
     </div>
   );
 }
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import axios from 'axios';
+import Technologies from './Technologies';
 
 export class Countries extends Component {
+    state = {
+        countries: []
+    }
+
+    componentDidMount() {
+        axios.get(`https://restcountries.eu/rest/v2/all`)
+          .then(res => {
+            this.setState({countries: res.data})
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+    }
     render() {
         return (
             <div>
-                
+                {this.state.countries.map((anObjectMapped, index) => {
+                    return (
+                        <p key={anObjectMapped.name}>
+                            {anObjectMapped.name}
+                        </p>
+                    );
+                })}
+                <h3> Countires</h3>
             </div>
         )
     }
