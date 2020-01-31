2e82f4839701c3efb21ebb11818e014f1470b241
Faisal <faisalrana079@gmail.com>
2020-01-31 19:37:34 +0500

    add details of countries

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -19,14 +19,50 @@ export class Countries extends Component {
     render() {
         return (
             <div>
+                <div className="row">
+                    <div className="col-md-2">
+                        <b>Flag</b>
+                    </div>
+                    <div className="col-md-2">
+                        <b>Name</b>
+                    </div>
+                    <div className="col-md-2">
+                        <b>Capital</b>
+                    </div>
+                    <div className="col-md-2">
+                        <b>Country Code</b>
+                    </div>
+                    <div className="col-md-2">
+                        <b>Region</b>
+                    </div>
+                    <div className="col-md-2">
+                        <b>Population</b>
+                    </div>
+                </div>
                 {this.state.countries.map((anObjectMapped, index) => {
                     return (
-                        <p key={anObjectMapped.name}>
-                            {anObjectMapped.name}
-                        </p>
+                        <div className="row">
+                            <div className="col-md-2" key={anObjectMapped.flag}>
+                                <img src={anObjectMapped.flag} width="25px"></img>
+                            </div>
+                            <div className="col-md-2" key={anObjectMapped.name}>
+                                {anObjectMapped.name}
+                            </div>
+                            <div className="col-md-2" key={anObjectMapped.capital}>
+                                {anObjectMapped.capital}
+                            </div>
+                            <div className="col-md-2" key={anObjectMapped.callingCodes}>
+                                {anObjectMapped.callingCodes}
+                            </div>
+                            <div className="col-md-2" key={anObjectMapped.region}>
+                                {anObjectMapped.region}
+                            </div>
+                            <div className="col-md-2" key={anObjectMapped.population}>
+                                {anObjectMapped.population}
+                            </div>
+                        </div>
                     );
                 })}
-                <h3> Countires</h3>
             </div>
         )
     }
