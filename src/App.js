9da317ae12e9f8a896859dfd048f0adf42ff48a4
Faisal <faisalrana079@gmail.com>
2020-01-31 18:43:47 +0500

    Add routes and integrate countries api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import './App.css';
 import BootStrap from 'bootstrap/dist/css/bootstrap.css';
-import FontAwesome from 'react-fontawesome'
-import Header from './components/Header';
 import Intro from './components/Intro';
 import Technologies from './components/Technologies';
 import Article from './components/Article';
-import Countries from './components/Countries';
 
 function App() {
   return (
     <div className="App">
-      <Header />
       <Intro />
       <Technologies />
       <Article />
-      <Countries />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,47 +2,57 @@ import React, { Component } from 'react'
 import './css/header.scss';
 import FontAwesome from 'react-fontawesome';
 import 'font-awesome/css/font-awesome.min.css';
+import Countries from './Countries'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import App from '../App';
 
 export class Header extends Component {
     render() {
         return (
-            <div className="container container-height">
-                <div className= "row">
-                    <div className= "col-md-3">
-                        <h4>TechM4</h4>
-                    </div>
-                    <div className= "col-md-3 columns">
-                        <span>
-                            <i className="fas fa-phone-alt"></i>
-                            <b>+ (123) 1800-567-8990</b>
-                        </span>
-                        <br/>
-                        <span>+ (123) 1800-567-8990</span>
-                    </div>
-                    <div className= "col-md-3 columns border-left">
-                        <span>
-                            <b>Mon - Fri: 9:00AM - 5:00PM</b>
-                        </span>
-                        <br/>
-                        <span>Sat - Sun: Closed</span>
-                    </div>
-                    <div className= "col-md-3 columns border-left">
-                        <span>
-                            <b>office@example.com</b>
-                        </span>
-                        <br/>
-                        <span>info@medical.com</span>
-                    </div>
+            <Router>
+                <div className="container container-height">
+                        <div className= "row">
+                            <div className= "col-md-3">
+                                <h4>TechM4</h4>
+                            </div>
+                            <div className= "col-md-3 columns">
+                                <span>
+                                    <i className="fas fa-phone-alt"></i>
+                                    <b>+ (123) 1800-567-8990</b>
+                                </span>
+                                <br/>
+                                <span>+ (123) 1800-567-8990</span>
+                            </div>
+                            <div className= "col-md-3 columns border-left">
+                                <span>
+                                    <b>Mon - Fri: 9:00AM - 5:00PM</b>
+                                </span>
+                                <br/>
+                                <span>Sat - Sun: Closed</span>
+                            </div>
+                            <div className= "col-md-3 columns border-left">
+                                <span>
+                                    <b>office@example.com</b>
+                                </span>
+                                <br/>
+                                <span>info@medical.com</span>
+                            </div>
+                        </div>
+                        <hr/>
+                        <div className="row menu">
+                            <a href="#">Home</a>
+                            <a href="#">About Us</a>
+                            <a href="#">Contact</a>
+                            <a href="#">Demo</a>
+                            <button className="contact-btn">Contacts</button>
+                        
+                        </div>
+                        <Link to="/">Home</Link>
+                        <Link to="/countries">Countries</Link>
+                        <Route exact path="/" component={App} />
+                        <Route path="/countries" component={Countries} />
                 </div>
-                <hr/>
-                <div className="row menu">
-                    <a href="#">Home</a>
-                    <a href="#">About Us</a>
-                    <a href="#">Contact</a>
-                    <a href="#">Demo</a>
-                    <button className="contact-btn">Contacts</button>
-                </div>
-            </div>
+            </Router>
         )
     }
 }
